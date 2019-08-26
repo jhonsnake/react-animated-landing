3a184f07a3469bdf767a066e7681377accd85442
John Prada <jhonprada@gmail.com>
2019-08-26 12:05:29 -0500

    Created Hero Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => (<Layout>
+    <SEO title="Home"/>
+    <div className="Hero">
+        <div className="HeroGroup">
+            <h1>Learn to design and code react apps</h1>
+            <p>Complete course about the besttools and design systems. Prototype and build apps with React and Swift</p>
+            <Link to="/page-2/">Watch the video</Link>
+        </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+</Layout>)
 
 export default IndexPage
